88bcc4502798a6c1ab6c2f2dc141b1aa55359c3f
Alexandre Perez <alexandre.perez@edu.devinci.fr>
2020-03-18 13:10:18 +0100

    Rectifications commit Ajout erreur 404

--- server/index.js
+++ server/index.js
@@ -9,12 +9,9 @@ app.get('/api/version', function getVersion (req, res) {
   res.send('0.0.2')
 })
 
-// Reste à faire ça :
-// Si on arrive ici, on n'a plus de route, donc on doit afficher une 404
-
-app.use(express.static('../client'), function (req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
-  res.sendFile(path.join(__dirname, '../client/404.html'))
+  res.sendFile(path.join(__dirname, '../client/dist/404.html'))
 })
 
 app.listen(port, () => console.log(`Listening ! Go to http://localhost:${port}!`))
